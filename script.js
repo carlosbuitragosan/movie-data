e5b58f02a0791acd55dc02bd18a7a3e3975b48ba
Carlos Buitrago <letscatchupagain@gmail.com>
2023-02-14 13:07:35 +0000

    fix -  object changed to array-had to redo code

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-const movieTitlesArray = Object.keys(movieData);
+let movieTitlesArray = Object.keys(movieData);
 const movieCard = document.querySelector('.movie');
-
-const displayMovieCard = () => {
-    movieTitlesArray.forEach(movie => {
-        movieCard.insertAdjacentHTML('beforeend',
-        `<h2 class="movie__title">${movie}</h2>
-        <h3 class="plot">Plot</h3>
-        <p class="plot__description">${movieData[movie].plot}</p>
-        <h3 class="cast"></h3>
-        <p class="cast__description">${movieData[movie].cast.join(', ')}.</p>
-        <h3 class="runtime">Runtime</h3>
-        <p class="runtime__description">${movieData[movie].runtime} min.</p>
-        <h3 class="rating">Rating</h3>
-        <p class="rating__descriptioin">${movieData[movie].rating}</p>
-        <h3 class="year">Year</h3>
-        <p class="year__description">${movieData[movie].year}</p>
-        `);
-    });
-};
-displayMovieCard();
+const movieDataArray = Object.entries(movieData);
 
 const form = document.querySelector('#movie__form')
 const title = document.querySelector('#title');
@@ -28,33 +10,60 @@ const rating = document.querySelector('#rating');
 const runtime = document.querySelector('#runtime');
 const cast = document.querySelector('#cast');
 
-// const addMovie = (key, value) => {
-//     return movieData[key] = value;
-//   }
+
+
+
+    //   **** add new movie *****
+const addMovie = (array) => {
+    movieDataArray.push(array);
+    return movieDataArray;
+}
 form.addEventListener('submit', event => {
     event.preventDefault();
-    movieCard.insertAdjacentHTML('beforeend',
-        `<h2 class="movie__title">${title.value}</h2>
-        <h3 class="plot">Plot</h3>
-        <p class="plot__description">${plot.value}</p>
-        <h3 class="cast"></h3>
-        <p class="cast__description">${cast.value}.</p>
-        <h3 class="runtime">Runtime</h3>
-        <p class="runtime__description">${runtime.value} min.</p>
-        <h3 class="rating">Rating</h3>
-        <p class="rating__descriptioin">${rating.value}</p>
-        <h3 class="year">Year</h3>
-        <p class="year__description">${year.value}</p>
-        `);
-    // addMovie(`${title.value}`, {
-    //     rating: `${rating.value}`, 
-    //     runtime: `${runtime.value}`,
-    //     year: `${year.value}`,
-    //     plot: `${plot.value}`,
-    //     cast: [`${cast.value}`]
-    //   })
+    addMovie([title.value, {
+        plot: plot.value,
+        cast: [cast.value],
+        runtime: runtime.value,
+        rating: rating.value,
+        year: year.value
+    }])
+    title.value = '';
+    plot.value = '';
+    cast.value = '';
+    runtime.value = '';
+    rating.value = '';
+    year.value = '';
+    render();
 });
 
 
+//    ***** render movies  *******
+const render = () => {
+    movieCard.innerHTML = '';
+    movieDataArray.forEach(movie => {
+        movieCard.insertAdjacentHTML('beforeend',
+        `<h2 class="movie__title">${movie[0]}</h2>
+        <h3 class="plot">Plot</h3>
+        <p class="plot__description">${movie[1].plot}</p>
+        <h3 class="cast">Cast</h3>
+        <p class="cast__description">${movie[1].cast.join(', ')}.</p>
+        <h3 class="runtime">Runtime</h3>
+        <p class="runtime__description">${movie[1].runtime} min.</p>
+        <h3 class="rating">Rating</h3>
+        <p class="rating__description">${movie[1].rating}</p>
+        <h3 class="year">Year</h3>
+        <p class="year__description">${movie[1].year}</p> 
+        `);
+    });
+};
 
-  
+//    *****   SORT MOVIES BY NAME  *****
+
+const sortByName = document.querySelector('.sort-by-name__button');
+sortByName.addEventListener('click', () => {
+    console.log(movieDataArray)
+    movieDataArray.sort();
+    render();
+})
+
+render();
