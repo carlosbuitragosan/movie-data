2b7040f1284fd6640501d05dd8d5e19d8e781bf7
Carlos Buitrago <letscatchupagain@gmail.com>
2023-03-25 16:20:48 +0000

    edit - fonts were too big

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const movieDataArray = Object.entries(movieData);
 
 
 
-//   ***** FORM SELECTORS  ******
+////////////////// ** FORM SELECTORS  *** //////////////////////////
 const form = document.querySelector('#movie__form')
 const title = document.querySelector('#title');
 const year = document.querySelector('#year');
@@ -14,19 +14,47 @@ const runtime = document.querySelector('#runtime');
 const cast = document.querySelector('#cast');
 const successMessage = document.querySelector('.message__saved');
 const enterInfo = document.querySelector('.enter__info');
+/////////
 
 
 
-    //   **** add new movie *****
+/////////////////////// *** RENDER MOVIES *** /////////////////////
+const render = () => {
+    movieCard.innerHTML = '';
+    movieDataArray.forEach(movie => {
+        movieCard.insertAdjacentHTML('beforeend',
+        `<div class="movie__each">
+        <h2 class="movie__title">${movie[0]}</h2>
+        <h3 class="plot">PLOT</h3>
+        <p class="plot__description">${movie[1].plot}</p>
+        <h3 class="cast">CAST</h3>
+        <p class="cast__description">${movie[1].cast.join(', ')}.</p>
+        <h3 class="runtime">RUNTIME</h3>
+        <p class="runtime__description">${movie[1].runtime} min.</p>
+        <h3 class="rating">RATING</h3>
+        <p class="rating__description">${movie[1].rating}</p>
+        <h3 class="year">YEAR</h3>
+        <p class="year__description">${movie[1].year}</p>
+        </div>
+        <hr>`);
+    });
+};
+/////////////
+
+
+
+
+/////////////////////// *** ADD NEW MOVIE *** ///////////////////////
 const addMovie = (array) => {
     movieDataArray.push(array);
     return movieDataArray;
+    render();
 };
 
 form.addEventListener('submit', event => {
     event.preventDefault();
 
-        // **** making sure all fields are filled in. if not show message *****
+        // **** making sure all fields are filled in. if not, show message *****
     if (title.value==='' || plot.value==='' || cast.value==='' || runtime.value==='' || rating.value==='' || year.value==='') {
         enterInfo.style.display = 'block';
         setTimeout(() => {
@@ -60,47 +88,25 @@ form.addEventListener('submit', event => {
         setTimeout(() => {
             form.classList.toggle('form__toggle_view');
         }, 2500);
-        // ****** after movie is added page re-renders ******
-        render();
     };
 });
-
-
-//    ***** render movies  *******
-const render = () => {
-    movieCard.innerHTML = '';
-    movieDataArray.forEach(movie => {
-        movieCard.insertAdjacentHTML('beforeend',
-        `<div class="movie__each">
-        <h2 class="movie__title">${movie[0]}</h2>
-        <h3 class="plot">PLOT</h3>
-        <p class="plot__description">${movie[1].plot}</p>
-        <h3 class="cast">CAST</h3>
-        <p class="cast__description">${movie[1].cast.join(', ')}.</p>
-        <h3 class="runtime">RUNTIME</h3>
-        <p class="runtime__description">${movie[1].runtime} min.</p>
-        <h3 class="rating">RATING</h3>
-        <p class="rating__description">${movie[1].rating}</p>
-        <h3 class="year">YEAR</h3>
-        <p class="year__description">${movie[1].year}</p>
-        </div>
-        <hr>`);
-    });
-};
+///////////
 
 
 
-//    *****   SORT MOVIES BY NAME  *****
+
+///////////////////// ***   SORT MOVIES BY NAME  *** ////////////////////
 const sortByName = document.querySelector('.sort-by-name__button');
 sortByName.addEventListener('click', () => {
     movieDataArray.sort();
     render();
 });
+////////////
 
 
 
 
-//    **** SORT MOVIES BY YEAR ****
+///////////////////// *** SORT MOVIES BY YEAR *** //////////////////////////
 const sortByYear = document.querySelector('.sort-by-year__button');
 
 sortByYear.addEventListener('click', () => {
@@ -109,11 +115,12 @@ sortByYear.addEventListener('click', () => {
     });
     render();
 });
+///////////
 
 
 
 
-    //  ***** SORT BY RATING  ******
+/////////////////// *** SORT BY RATING  *** //////////////////////////////////
 const sortByRating = document.querySelector('.sort-by-rating__button');
 
 sortByRating.addEventListener('click', () => {
@@ -122,23 +129,31 @@ sortByRating.addEventListener('click', () => {
     });
     render();
 })
+///////////
 
 
 
-//    *****   DROPDOWN MENU *****
+
+//////////////////// ***   DROPDOWN MENU *** ////////////////////////////
 const headerSortButton = document.querySelector('.header__sort');
 const menuList = document.querySelector('.menu__toggle_view');
 
 headerSortButton.addEventListener('click', () => {
     menuList.classList.toggle('menu__toggle_view')
 });
+///////
 
 
-    // **** DROPDOWN FORM *****
+
+
+///////////////////// *** DROPDOWN FORM *** ///////////////////////////////
 const formTitleButton = document.querySelector('.form__title_button');
 formTitleButton.addEventListener('click', () => {
     form.classList.toggle('form__toggle_view');
 });
+//////////
+
+
 
 
 render();
